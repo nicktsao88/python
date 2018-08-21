3bf0dc13108c4c3e9a954e4956746df6726a06ed
nicktsao88 <nicktsao88@gmail.com>
2018-08-21 16:42:01 +0800

    “計算”

---
+++ nick6.py
@@ -0,0 +1,80 @@
+def add(a,b) :
+    print(a+b)
+def remove(a,b) :
+    print(a-b)
+def mulity(a,b) :
+    print(a*b)
+def dived(a,b) :
+    print(a/b)
+def s(a,b) :
+    print(a**b)
+def a(a,b) :
+    print(a**(1/b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
